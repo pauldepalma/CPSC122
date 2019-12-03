fc8ca888518f8782fac5d2bc99d68ab3e084ac63
Paul De Palma <depalma@gonzaga.edu>
2019-12-03 15:34:16 -0800

    Priority Queue

--- Queues/queue3.h
+++ Queues/queue3.h
@@ -19,6 +19,14 @@ class PQueue : public Queue
    post: A new node is added to PQueue, which remains in sorted order 
    */
    void Enqueue(itemType newItem);
+ private:
+   /*
+   pre: an instance of PQueue  exists. nodes in queue are in ascending order by 
+        item.  Is not used if item < head->item or item > tail->item
+   post: returns the address of nodeA such that nodeA->item < newItem and
+        nodeA->next->item > newItem
+   */
+   node* ptrTo(itemType item);
 
 };
 #endif
