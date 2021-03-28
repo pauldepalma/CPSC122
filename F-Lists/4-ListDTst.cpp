54d27af1602d83eb895d46a00cc1050047835b31
Paul De Palma <you@example.com>
2021-03-27 17:03:41 -0700

    Unit Test

--- F-Lists/4-ListDTst.cpp
+++ F-Lists/4-ListDTst.cpp
@@ -3,21 +3,43 @@ using namespace std;
 
 #include "4-ListD.h"
 
+void TestInsert();
 
 int main()
 {
+ TestInsert();
+}
+
+void TestInsert()
+{
+
  ListD* lst = new ListD;
- 
- for (int i = 1; i <= 10; i++)
-   lst->Insert(i,i);
+ for (int i = 1; i <= 6; i++)
+  lst->Insert(i,i);
 
+ //test general case insert
+ cout << "Passed Insert Test 1 if 1 through 6 appear on subsequent lines" << endl;
  lst->PrintForward();
+ cout << endl;
 
+ //test insert at the head
+ lst->Insert(0,1);
+ cout << "Passed Insert Test 2 if 0 appears in position 1" << endl; 
+ lst->PrintForward();
+ cout << endl;
+
+ //test insert at the tail 
+ lst->Insert(100,8);
+ cout << "Passed Insert Test 3 if 100 appears in final position" << endl; 
+ lst->PrintForward();
  cout << endl;
  
- ListD* lst1 = new ListD(lst);
-
- lst1->PrintForward();
-
- return 0;
+ //test insert in middle 
+ lst->Insert(50,5);
+ cout << "Passed Insert Test 4 if 50 appears in middle position" << endl; 
+ lst->PrintForward();
+ cout << endl;
+ 
+ delete lst;
 }
+
