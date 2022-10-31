570fa2c36e122094692cdabf35cd29394964c0b9
Paul De Palma <depalma@gonzaga.edu>
2022-10-31 09:39:15 -0700

    testing

---
+++ F-Lists/TestingExamples.cpp
@@ -0,0 +1,135 @@
+#include <iostream>
+using namespace std;
+
+#include "2-List.h"
+
+int main()
+{
+
+ //List* lst1 = new List;
+ //List lst;
+ /*
+ cout << "IsEmpty test 1" << endl;
+ cout << "Output 1" << endl;
+ cout << lst1->IsEmpty() << endl; 
+ cout << endl;
+ */
+
+ /*
+ cout << "GetLength test 1" << endl;
+ cout << "Output 0" << endl;
+ cout << lst1->GetLength() << endl; 
+ cout << endl;
+ */
+
+ //cout << "PutItemH 1 thorugh 5 Static List" << endl;
+ //cout << "Output 5 through 1" << endl;
+ //for (int i = 1; i < 6; i++)
+ // lst.PutItemH(i);
+ //lst.Print();
+
+ //cout << "PutItemH 1 thorugh 5 Dynamic List" << endl;
+ //cout << "Output 5 through 1" << endl;
+ //for (int i = 1; i < 6; i++)
+ // lst1->PutItemH(i);
+ //lst1->Print();
+
+ /*
+ cout << "IsEmpty test 2" << endl;
+ cout << "Output 0" << endl;
+ cout << lst1->IsEmpty() << endl; 
+ cout << endl;
+ */
+
+ /*
+ cout << "GetLength test 2" << endl;
+ cout << "Output 5" << endl;
+ cout << lst1->GetLength() << endl; 
+ cout << endl;
+ */
+
+ /* 
+ cout << "GetItemH test 1" << endl;
+ cout << "Output 5" << endl;
+ cout << lst1->GetItemH()<< endl; 
+ cout << endl;
+ */
+
+ /*
+ cout << "Print test 2" << endl;
+ cout << "Output 5 through 1" << endl;
+ lst1->Print();
+ cout << endl;
+ */
+
+ /*
+ cout << "DeleteItemH test 1" << endl;
+ cout << "Output 4 through 1" << endl;
+ lst1->DeleteItemH();
+ lst1->Print(); 
+ cout << endl;
+ */
+
+ /*
+ cout << "Find tests 1 through 4" << endl;
+ lst1->Print();
+ cout << "Output 1 " << endl;
+ cout << lst1->Find(5) << endl;
+ cout << "Output 3" << endl;
+ cout << lst1->Find(3) << endl;
+ cout << "Output 5" << endl;
+ cout << lst1->Find(1) << endl;
+ cout << "Output -1" << endl; cout << lst1->Find(0) << endl;
+ cout << endl;
+
+ delete lst1;
+ */
+
+ /*
+ cout << "DeleteItem tests" << endl;
+ List* lst2 = new List;
+ cout << "target is at head and elsewhere" << endl;
+ cout << "output should be 2 0 2 3 2 0 3" << endl; 
+ lst2->PutItemH(3);
+ lst2->PutItemH(2);
+ lst2->PutItemH(0);
+ lst2->PutItemH(2);
+ lst2->Print();
+ cout << endl;
+ cout << lst2->DeleteItem(2) << endl;
+ cout << endl;
+ lst2->Print();
+ cout << endl;
+ 
+ cout << "target is followed by a target" << endl;
+ cout << "output should be 2 1 0 3" << endl;
+ lst2->PutItemH(2);
+ lst2->PutItemH(2);
+ lst2->PutItemH(1);
+ cout << lst2->DeleteItem(2) << endl;
+ cout << endl;
+ lst2->Print();
+ cout << endl;
+
+ cout << "target is at tail" << endl;
+ cout << "output should be 1 1 0" << endl;
+ cout << lst2->DeleteItem(3) << endl;
+ cout << endl;
+ lst2->Print();
+
+ cout << "target is at tail and just previous to tail" << endl;
+ cout << "output should be 2 0" << endl;
+ lst2->DeleteItem(0);
+ lst2->DeleteItem(3);
+ cout << endl;
+ lst2->PutItemH(1);
+ lst2->PutItemH(0);
+ cout << endl;
+ cout << lst2->DeleteItem(1) << endl;
+ cout << endl;
+ lst2->Print(); 
+ delete lst2;
+*/
+
+ return 0;
+}
