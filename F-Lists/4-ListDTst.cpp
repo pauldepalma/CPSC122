c75ed65e8a411a639e42d24cc4eb2c8c5f9e867a
Paul De Palma <depalma@gonzaga.edu>
2022-11-03 18:31:52 -0700

    ListD

--- F-Lists/4-ListDTst.cpp
+++ F-Lists/4-ListDTst.cpp
@@ -3,14 +3,14 @@ using namespace std;
 
 #include "4-ListD.h"
 
-//All tests are separated by function
+//All tests are controlled by a dedicated function
 void TestInsert();
 void TestCopyConstructor();
 
 int main()
 {
- TestInsert();
- //TestCopyConstructor();
+ //TestInsert();
+ TestCopyConstructor();
 }
 
 
@@ -21,26 +21,25 @@ void TestInsert()
  ListD* lst = new ListD;
  for (int i = 1; i <= 5; i++)
   lst->Insert(i,i);
- lst->PrintForward();
 
- //test general case insert
+ cout << "test general case insert" << endl;
  cout << "Passed Insert Test 1 if 1 through 5 appear on subsequent lines" << endl;
  lst->PrintForward();
  cout << endl;
 
- //test insert at the head
+ cout << "test insert at the head" << endl;
  lst->Insert(0,1);
  cout << "Passed Insert Test 2 if 0 appears in position 1" << endl; 
  lst->PrintForward();
  cout << endl;
 
- //test insert at the tail 
+ cout << "test insert at the tail" << endl; 
  lst->Insert(100,7);
  cout << "Passed Insert Test 3 if 100 appears in final position" << endl; 
  lst->PrintForward();
  cout << endl;
  
- //test insert in middle 
+ cout << "test insert within the list" << endl; 
  lst->Insert(50,5);
  cout << "Passed Insert Test 4 if 50 appears in middle position" << endl; 
  lst->PrintForward();
@@ -55,11 +54,13 @@ void TestCopyConstructor()
   lst1->Insert(i,i);
 
  ListD* lst2(lst1);
- 
- cout << "Traverse lst1" << endl;
+
+ cout << "Test Copy Constructor" << endl; 
+ cout << "Test passed if copy is indentical to initial list" << endl; 
+ cout << "Traverse inital list" << endl;
  lst1->PrintForward();
  cout << endl;
- cout << "Traverse lst2" << endl;
+ cout << "Traverse copy" << endl;
  lst2->PrintForward();
 
  //delete lst1;
