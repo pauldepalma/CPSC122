9c1cc391ae9b76ba57a0e19aa8112991a2ba0eba
Paul De Palma <depalma@gonzaga.edu>
2025-10-06 14:54:50 -0700

    fix MI

--- C-Sort-Search/4-dict.cpp
+++ C-Sort-Search/4-dict.cpp
@@ -11,7 +11,7 @@ Description: illustrates using an array as a dictionary of multiplicative invers
 #include <iostream>
 #include <cstdlib>
 
-int DICT[26] = {0,1,0,9,0,21,0,15,0,0,0,19,0,0,0,7,0,23,0,11,0,5,0,17,0,25};
+int DICT[26] = {0,1,0,9,0,21,0,15,0,3,0,19,0,0,0,7,0,23,0,11,0,5,0,17,0,25};
 
 int getInv(int); 
 
